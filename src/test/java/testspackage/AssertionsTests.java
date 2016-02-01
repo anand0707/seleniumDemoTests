bb65e67f0e961414e754fc9cf901e5bd8270d802
Anand Bajpai <anand.bajpai@ril.com>
2016-02-01 12:05:32 +0530

    testing2

---
+++ src/test/java/testspackage/AssertionsTests.java
@@ -0,0 +1,43 @@
+package testspackage;
+
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class AssertionsTests {
+	 @Test
+	   public void test1() {
+	      String str = "TestNG is working fine";
+	      assertEquals("TestNG is working fine", str);
+	   }
+	 
+	 @Test
+	   public void test2() {
+	      String str = "TestNG is working fine";
+	      assertEquals("TestNG is working fine", str);
+	   }
+	 
+	 @Test
+	   public void test3() {
+	      String str = "TestNG is working fine";
+	      assertEquals("TestNG is working fine", str);
+	   }
+	 
+	 @Test
+	   public void test4() {
+	      String str = "TestNG is working fine";
+	      assertEquals("TestNG is working fine", str);
+	   }
+	 
+	 @Test
+	   public void test5() {
+	      String str = "TestNG is working fine";
+	      assertEquals("TestNG is working fine", str);
+	   }
+	 
+	 @Test
+	   public void test6() {
+	      String str = "TestNG is working fine";
+	      assertEquals("TestNG is working fine", str);
+	   }
+}
